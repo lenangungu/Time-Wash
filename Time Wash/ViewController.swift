9355edef6344195ed364d9fdfb5b1eec0ca1ca43
Lena Ngungu <lenangungu@hotmail.co.uk>
2016-11-12 22:07:50 -0600

    Initial Commit

---
+++ Time Wash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Time Wash
+//
+//  Created by Lena Ngungu on 11/12/16.
+//  Copyright © 2016 Lena Ngungu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
